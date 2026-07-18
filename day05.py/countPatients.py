ac913844104e407a5842b12b2419437dcd31d447
Thalia Olvera <thalia.olvera.delarosa@gmail.com>
2026-07-17 20:48:29 -0500

    update bmi function

--- day05.py/countPatients.py
+++ day05.py/countPatients.py
@@ -47,6 +47,29 @@ def min_bmi():
     bmis=[patient["bmi"] for patient in patients ]
     return min(bmis)
 
-print("The bmi averange is: ", bmi_averange())
-print("Max value bmi : ", max_bmi())
-print("Minimun value bmi : ", min_bmi())
+user_info=input("Write de patient's name : ")
+user_update=float(input("Write de new weight in kg: "))
+
+def update_weight(user_info, new_weight):
+    for patient in patients:
+        if patient["name"].lower() == user_info.lower():
+            patient["weight"]=new_weight
+            patient["bmi"]= calculate_bmi(patient["weight"],patient["height"])
+            return patient
+        
+
+def calculate_bmi(weight,height):
+        bmi= weight / (height*height)
+        return round(bmi, 2)
+    
+updated = update_weight(user_info,user_update)
+
+if updated:
+    print(f"Weight: {updated['weight']} kg")
+    print(f"BMI: {updated['bmi']} kg")
+else:
+    print("Patient not found.")
+
+#print("The bmi averange is: ", bmi_averange())
+#print("Max value bmi : ", max_bmi())
+#print("Minimun value bmi : ", min_bmi())
