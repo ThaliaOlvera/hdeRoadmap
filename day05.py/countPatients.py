ab5435ca6131a3d597848ece4384fb52ae3b818d
Thalia Olvera <thalia.olvera.delarosa@gmail.com>
2026-07-18 21:42:41 -0500

    function to eliminate patients

--- day05.py/countPatients.py
+++ day05.py/countPatients.py
@@ -29,11 +29,26 @@ patients = [
     }
 ]
 
+def bmi_clasification(bmi):
+      if bmi <18.5:
+           result ="underweight"
+      elif bmi >= 18.5 and bmi <= 24.9:
+           result ="normal weight"
+      elif bmi >=25 and bmi <= 29.9:
+           result ="overweight"
+      elif bmi >=30 and bmi <= 34.9:
+           result ="Class I Obesity"
+      elif bmi >=35 and bmi <= 39.9:
+           result ="Class II Obesity"
+      elif bmi >=40:
+           result ="lass III Obesity"
+      else:
+           print("Error")
+      return result
 
 def get_total_patients(patients):
     return len(patients)
 
-print("Total patients is : ", get_total_patients(patients))
 
 def bmi_averange():
     bmis=[patient["bmi"] for patient in patients ]
@@ -55,18 +70,41 @@ def update_weight(user_info, new_weight):
         if patient["name"].lower() == user_info.lower():
             patient["weight"]=new_weight
             patient["bmi"]= calculate_bmi(patient["weight"],patient["height"])
+            patient["clasification"]=bmi_clasification(patient["bmi"])
             return patient
         
 
 def calculate_bmi(weight,height):
         bmi= weight / (height*height)
         return round(bmi, 2)
-    
+
+
+def delete_patient(user_info):
+    for patient in patients:
+        if patient["name"].lower()==user_info.lower():
+          user_response=input("Do you want to delete this patient?")
+          if user_response.lower()== "yes":
+               patients.remove(patient)
+               return patient
+          else:
+               return None
+    print("Patient not found")
+    return None
+
+deleted = delete_patient(user_info)
+
+if deleted:
+    print("Patient deleted successfully.")
+else:
+    print("Patient not found or deletion canceled.")
+
 updated = update_weight(user_info,user_update)
+print("Total patients is : ", get_total_patients(patients))
 
 if updated:
     print(f"Weight: {updated['weight']} kg")
     print(f"BMI: {updated['bmi']} kg")
+    print(f"BMI Clasification: {updated['clasification']}")
 else:
     print("Patient not found.")
 
