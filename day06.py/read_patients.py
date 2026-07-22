f1ebc8c086a575db9ca2ab4b848daca811412c78
Thalia Olvera <thalia.olvera.delarosa@gmail.com>
2026-07-21 20:03:05 -0500

    read csv file

--- day06.py/read_patients.py
+++ day06.py/read_patients.py
@@ -1,5 +1,23 @@
 import csv
 
 with open("patients.csv", "r") as file:
-    content = file.read()
-    print(content)
+    reader = csv.DictReader(file)
+    
+    patients=[]
+
+    for patient in reader:
+        patients.append(patient)
+        
+
+for patient in patients:
+    print(f"Name: {patient['Name']}")
+    print(f"Age: {patient['Age']}")
+    print(f"Weight: {patient['Weight']}")
+    print()
+        
+
+
+      
+
+   
+
